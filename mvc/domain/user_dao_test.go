12268d0ee3c03d8b42309d774296bd98bafd6ed1
Puneet Singh <singh.puneet1997@gmail.com>
2021-01-28 11:09:10 +0530

    Adding tests for user dao (domain layer). (#2)
    * mvc: adding assert dependency for testing.
    
    * mvc: modify error message for user not found in domain layer.
    
    * mvc: adding tests for user dao.

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	user, err := GetUser(0)
+
+	assert.Nil(t, user, "we were not expecting a user with id 0")
+	assert.NotNil(t, err)
+
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 does not exists", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(1)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 1, user.ID)
+	assert.EqualValues(t, "Magnus", user.FirstName)
+	assert.EqualValues(t, "Carlsen", user.LastName)
+	assert.EqualValues(t, "mc@chess.com", user.Email)
+}
